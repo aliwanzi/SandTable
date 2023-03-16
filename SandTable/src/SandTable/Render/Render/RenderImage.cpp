97118204aa5a2294af235c677817810a2ac0c0e5
wenyu.han_HSW-GS <13052523498@163.com>
2023-03-16 20:12:37 +0800

    Render sphere

--- SandTable/src/SandTable/Render/Render/RenderImage.cpp
+++ SandTable/src/SandTable/Render/Render/RenderImage.cpp
@@ -29,6 +29,7 @@ void RenderImage::OnRender()
 		{
 			glm::vec2 vec2Coord = { static_cast<float>(x) / static_cast<float>(width),
 				static_cast<float>(y) / static_cast<float>(height) };
+			vec2Coord = vec2Coord * 2.f - 1.f; //(0~1)->(-1,1)
 			pImageData[x + y * width] = GetPerPixel(vec2Coord);
 		}
 	}
@@ -44,7 +45,23 @@ uint32_t RenderImage::GetPerPixel(const glm::vec2& vec2Coord)
 {
 	uint8_t r = static_cast<uint8_t>(vec2Coord.x * UCHAR_MAX);
 	uint8_t g = static_cast<uint8_t>(vec2Coord.y * UCHAR_MAX);
-	return 0xff000000 | (g << 8) | r;
+
+	glm::vec3 vec3RayOrigin(0.f, 0.f, -10.f);
+	glm::vec3 vec3Direction(vec2Coord.x, vec2Coord.y, -1.f);
+
+	float fRadius(0.5f);
+	float fA = glm::dot(vec3Direction, vec3Direction);
+	float fB = 2.f * glm::dot(vec3RayOrigin, vec3Direction);
+	float fC = glm::dot(vec3RayOrigin, vec3RayOrigin) - fRadius * fRadius;
+
+	float discriminant = fB * fB - 4.0f * fA * fC;
+
+	if (discriminant >= 0.f)
+	{
+		return 0xffff00ff;
+	}
+
+	return 0xff000000 ;
 }
 
 SAND_TABLE_NAMESPACE_END
